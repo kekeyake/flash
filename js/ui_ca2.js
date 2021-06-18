6c176635b20b6ee88bb086e492ada9c0149111cc
martin <martin@irm.kr>
2021-06-18 14:01:34 +0900

    ca2 세계의 현대 건축가 경로 수정

---
+++ js/ui_ca2.js
@@ -0,0 +1,41 @@
+$(function(){
+
+    //main thumb list img toggle 
+    $(".thumb_list img").on('mouseenter', function() {
+        $(this).attr("src", $(this).attr("src").replace(".jpg","_on.jpg"));        
+    }).on('mouseleave', function() {
+        $(this).attr("src", $(this).attr("src").replace("_on.jpg", ".jpg"));
+    });
+
+    $('#menu1').on('mouseenter', function(){
+        $("#menu01").css('visibility','visible');
+        $(this).attr("src", $(this).attr("src").replace("menu1.gif","menu1r.gif"));      
+    }).on('mouseleave',function() {
+        $("#menu01").css('visibility','hidden');
+        $(this).attr("src", $(this).attr("src").replace("menu1r.gif","menu1.gif"));      
+    });
+
+    $('#menu2').on('mouseenter', function(){
+        $("#menu02").css('visibility','visible');
+        $(this).attr("src", $(this).attr("src").replace("menu2.gif","menu2r.gif"));      
+    }).on('mouseleave',function() {        
+        $("#menu02").css('visibility','hidden');
+        $(this).attr("src", $(this).attr("src").replace("menu2r.gif","menu2.gif"));      
+    });
+
+    $('#menu01').on('mouseenter', function(){
+        $(this).css('visibility','visible');
+        $('#menu1').attr("src", $('#menu1').attr("src").replace("menu1.gif","menu1r.gif"));      
+    }).on('mouseleave',function() {
+        $(this).css('visibility','hidden');
+        $('#menu1').attr("src", $('#menu1').attr("src").replace("menu1r.gif","menu1.gif"));      
+    });
+
+    $('#menu02').on('mouseenter', function(){
+        $(this).css('visibility','visible');
+        $('#menu2').attr("src", $('#menu2').attr("src").replace("menu2.gif","menu2r.gif"));      
+    }).on('mouseleave',function() {
+        $(this).css('visibility','hidden');
+        $('#menu2').attr("src", $('#menu2').attr("src").replace("menu2r.gif","menu2.gif"));      
+    });
+});
